f3868c6a4e61009f3899414ab54b5e90cc8106bd
panjitamzil <panji.tamzil@ymail.com>
2019-08-18 17:29:46 +0700

    Update code create diagnose

--- app/Http/Controllers/DiagnoseDataController.php
+++ app/Http/Controllers/DiagnoseDataController.php
@@ -30,42 +30,8 @@ class DiagnoseDataController extends Controller
     }
 
     public function create (Request $request) {
-        $diagnose = new DiagnoseData;
-        $diagnose->no_police = $request->no_police;
-        $diagnose->car_model_id = $request->car_model_id;
-        $diagnose->customer_name = $request->customer_name;
-        $diagnose->delivery_date = $request->delivery_date;
-        $diagnose->transmisi_type_id = $request->transmisi_type_id;
-        $diagnose->problem_component = $request->problem_component;
-        $diagnose->since_at = $request->since_at;
-        $diagnose->frequency = $request->frequency;
-        $diagnose->engine_condition = $request->engine_condition;
-        $diagnose->position_shift_lever = $request->position_shift_lever;
-        $diagnose->speed = $request->speed;
-        $diagnose->weight = $request->weight;
-        $diagnose->engine_speed = $request->engine_speed;
-        $diagnose->total_passenger = $request->total_passenger;
-        $diagnose->odometer = $request->odometer;
-        $diagnose->road_condition = $request->road_condition;
-        $diagnose->traffic_condition = $request->traffic_condition;
-        $diagnose->weather_condition = $request->weather_condition;
-        $diagnose->outside_air_temperatur = $request->outside_air_temperatur;
-        $diagnose->time_of_occurance = $request->time_of_occurance;
-        $diagnose->ac_condition = $request->ac_condition;
-        $diagnose->detail_inspection = $request->detail_inspection;
-        $diagnose->dtc = $request->dtc;
-        $diagnose->status = $request->status;
-        $diagnose->main_analysis_problem = $request->main_analysis_problem;
-        $diagnose->job_instruction = $request->job_instruction;
-        $diagnose->foreman = $request->foreman;
-        $diagnose->video_link = $request->video_link;
-        $diagnose->need_dtr = $request->need_dtr;
-        $diagnose->photo = $request->file('photo');
-        $diagnose->save();
-
-        //Move Uploaded File
-        $destinationPath = 'uploads';
-        $diagnose->photo->move($destinationPath,$diagnose->photo->getClientOriginalName());
+        $data = $request->all();
+        $diagnose = DiagnoseData::create($data);
 
         return response()->json(
             [
